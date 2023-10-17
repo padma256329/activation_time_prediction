37ada90af44aa15e16281b9aa1b60a3abf0c318c
padmavathi <padmavathikora@ettma.com>
2023-10-17 08:31:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_time', methods=['POST'])
+def predict_time():
+    try:
+        int_features = [int(x) for x in request.form.values()]
+        final_features = [np.array(int_features)]
+        output = model.predict(final_features)
+        #output = round(output[0], 2)
+        return render_template('index.html', prediction_text='Predicted activation time {}'.format(output))
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
